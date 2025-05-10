c528e1202fbe7538958e64d6c2c1a2a858cea6c2
MahmoudFawzyHelmy <mahmoudfawzy.helmy@gmail.com>
2025-05-10 19:24:15 +0300

    final update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
+import React from "react";
+import ProfileCard from "./ProfileCard";
+import alexaImage from "./assets/alexa.png";
+import cortanaImage from "./assets/cortana.png";
+import siriImage from "./assets/siri.png";
+import { Col, Container, Row } from "react-bootstrap";
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React Starter Project</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.js</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container className="mt-5">
+      <h1 className="text-center">Personal Digital Assistants</h1>
+      <p className="text-center">This is a simple profile card component</p>
+      <Row>
+        <Col>
+          <ProfileCard title="Alexa" handle="alexa@99" img={alexaImage} />
+        </Col>
+        <Col>
+          <ProfileCard title="Cortana" handle="cortana@99" img={cortanaImage} />
+        </Col>
+        <Col>
+          <ProfileCard title="Siri" handle="siri@99" img={siriImage} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
-
-export default App;
---
+++ src/ProfileCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+export default function ProfileCard({ title, handle, img }) {
+  return (
+    <>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={img} alt="image for testing" />
+        <Card.Body>
+          <Card.Title> Title : {title}</Card.Title>
+          <Card.Text>{handle}</Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
+}

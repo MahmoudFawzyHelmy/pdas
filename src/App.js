a88e3755b8065a0ad465747aa42041393587bb25
MahmoudFawzyHelmy <mahmoudfawzy.helmy@gmail.com>
2025-05-11 02:50:40 +0300

    final editing

--- src/App.js
+++ src/App.js
@@ -5,18 +5,35 @@ import siriImage from "./assets/siri.png";
 import { Col, Container, Row } from "react-bootstrap";
 export default function App() {
   return (
-    <Container className="mt-5">
-      <h1 className="text-center">Personal Digital Assistants</h1>
-      <p className="text-center">This is a simple profile card component</p>
-      <Row>
+    <Container>
+      <div className="bg-primary text-white p-3 mb-4 rounded">
+        <h1 className="text-center">Personal Digital Assistants</h1>
+        <p className="text-center">This is a simple profile card component</p>
+      </div>
+      <Row className="m-auto g-4 text-center">
         <Col>
-          <ProfileCard title="Alexa" handle="alexa@99" img={alexaImage} />
+          <ProfileCard
+            title="Alexa"
+            handle="alexa@99"
+            img={alexaImage}
+            description="Alexa was created by Amazon and helps you buy things."
+          />
         </Col>
         <Col>
-          <ProfileCard title="Cortana" handle="cortana@99" img={cortanaImage} />
+          <ProfileCard
+            title="Cortana"
+            handle="cortana@99"
+            img={cortanaImage}
+            description="Cortana was made by Microsoft. Who knows what it does?"
+          />
         </Col>
         <Col>
-          <ProfileCard title="Siri" handle="siri@99" img={siriImage} />
+          <ProfileCard
+            title="Siri"
+            handle="siri@99"
+            img={siriImage}
+            description="Siri was made by Apple and is being phased out"
+          />
         </Col>
       </Row>
     </Container>
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-export default function ProfileCard({ title, handle, img }) {
+export default function ProfileCard({ title, handle, img, description }) {
   return (
     <>
       <Card style={{ width: "18rem" }} className="shadow-lg">
@@ -10,6 +10,7 @@ export default function ProfileCard({ title, handle, img }) {
         <Card.Body>
           <Card.Title> Title : {title}</Card.Title>
           <Card.Text>{handle}</Card.Text>
+          <Card.Text>{description}</Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
